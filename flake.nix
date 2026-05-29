3425201a02f843936197a445b7f5a23e2c3eedc6
Jess <jessforster08@yahoo.com>
2026-05-29 10:58:40 -0300

    init: project scaffold

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "threadbare - lightweight email client for Linux";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rustup
+            pkg-config
+            gtk4
+            libadwaita
+            glib
+            sqlite
+            openssl
+            git
+          ];
+
+          shellHook = ''
+            echo "threadbare development environment loaded"
+            rustup default stable
+          '';
+        };
+      }
+    );
+}
+
